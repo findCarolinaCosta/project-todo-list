cf02e84ed31a77590eb1cd8dfd527357fc5d7097
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-07 23:43:05 -0300

    Refatora posicionamento e alguns detalhes funcionais

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
 const inputTypeTask = document.getElementById('texto-tarefa');
 const createTaskBtn = document.getElementById('criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const itemList = document.getElementsByClassName('item-list');
+const lis = document.getElementsByTagName('li');
 
 function createTask() {
   createTaskBtn.addEventListener('click', () => {
     const createItem = document.createElement('li');
+    const span = document.createElement('span');
+    const txt = document.createTextNode('\u00D7');
     taskList.appendChild(createItem);
     createItem.innerText = inputTypeTask.value;
     inputTypeTask.value = '';
-    createItem.innerHTML = "<label class='container'><input type='checkbox' class='checkbox'> <span class='checkmark'></span>" + createItem.innerText + "</label>";
-    createItem.onclick = function (event) { event.path[0].checked ? createItem.classList.toggle('completed') : createItem.classList.remove('completed') }
+    createItem.classList = 'item-list';
+    createItem.innerHTML = "<label class='container'><input type='checkbox' class='checkbox'><span class='checkmark'></span><p>" + createItem.innerText + "</p></label>";
+    span.className = 'close';
+    span.appendChild(txt);
+    createItem.appendChild(span);
+    createItem.onclick = function (event) {
+      event.path[0].checked ? createItem.classList.toggle('completed') : createItem.classList.remove('completed');
+      createItem.addEventListener('dblclick', (event) => {
+        getSelected(event);
+      });
+    }
+    getItemDisplayNone();
   });
 }
 createTask();
 
-taskList.addEventListener('click', (event) => {
+
+const getSelected = (event) => {
   const select = document.querySelectorAll('.selected');
   for (let under = 0; under < select.length; under += 1) {
     select[under].classList.remove('selected');
     select[under].style.backgroundColor = '';
   }
-  event.target.classList.add('selected');
+  event.path[2].classList.add('selected');
   const getSelected = document.querySelector('.selected');
   getSelected.style.backgroundColor = 'rgb(128, 128, 128)';
-});
+}
+
+const getChangesTask = () => {
+  if (localStorage.getItem('List') !== null) {
+    console.log('entrou');
+    localStorage.removeItem('List');
+    localStorage.setItem('List', taskList.innerHTML);
+  }
+}
+
+const getItemDisplayNone = () => {
+  const close = document.getElementsByClassName("close");
+  let i;
+  for (i = 0; i < close.length; i += 1) {
+    close[i].onclick = function () {
+      const li = this.parentElement;
+      li.parentNode.removeChild(li);
+      getChangesTask();
+    }
+  }
+}
 
 const btnDeleteAllList = document.getElementById('apaga-tudo');
 function deleteAllList() {
-  while (taskList.firstChild) {
-    taskList.removeChild(taskList.lastChild);
+  if (window.confirm('Você tem certeza que quer apagar todas as tarefas?')) {
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.lastChild);
+    }
+    localStorage.removeItem('List');
   }
 }
 btnDeleteAllList.addEventListener('click', deleteAllList);
@@ -40,17 +78,18 @@ function rmTaskDone() {
   for (let i = 0; i < completed.length; i += 1) {
     completed[i].remove();
   }
+  getChangesTask();
 }
 btnRmDone.addEventListener('click', rmTaskDone);
 
 const btnSaveTask = document.getElementById('salvar-tarefas');
 function saveTask() {
-  localStorage.setItem(1, taskList.innerHTML);
+  localStorage.setItem('List', taskList.innerHTML);
 }
 btnSaveTask.addEventListener('click', saveTask);
 
 function loadSaveTask() {
-  taskList.innerHTML = localStorage.getItem(1);
+  taskList.innerHTML = localStorage.getItem('List');
 }
 loadSaveTask();
 
@@ -88,8 +127,10 @@ btnMoveDown.addEventListener('click', () => {
 
 const btnDeleteSelected = document.getElementById('remover-selecionado');
 btnDeleteSelected.addEventListener('click', () => {
-  const taskSected = document.querySelectorAll('.selected');
-  for (let until = 0; until < taskSected.length; until += 1) {
-    taskSected[until].remove();
+  const selected = document.getElementsByClassName('selected');
+  selected[0].classList.add('animate__animated', 'animate__backOutRight');
+  for (let until = 0; until < selected.length; until += 1) {
+    selected[until].remove();
   }
+  getChangesTask();
 });
